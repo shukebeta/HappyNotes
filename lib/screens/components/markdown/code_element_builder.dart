c8c8328fb994adccc45f7daa521277de539b36eb
DW at xps <weizhong2004@gmail.com>
2024-07-25 16:07:51 +1200

    "fix: adjust code block rendering with multiple lines"

--- lib/screens/components/markdown/code_element_builder.dart
+++ lib/screens/components/markdown/code_element_builder.dart
@@ -3,13 +3,13 @@ import 'package:flutter_markdown_selectionarea/flutter_markdown.dart';
 import 'package:markdown/markdown.dart' as md;
 
 class CodeElementBuilder extends MarkdownElementBuilder {
-  bool _isMutipleLine(md.Element element) {
+  bool _isMultipleLine(md.Element element) {
     return element.attributes['class'] != null || element.textContent.contains("\n");
   }
 
   @override
   Widget? visitElementAfter(md.Element element, TextStyle? preferredStyle) {
-    if (_isMutipleLine(element)) {
+    if (_isMultipleLine(element)) {
       return Container(
         margin: const EdgeInsets.fromLTRB(7, 20, 10, 0),
         width: MediaQueryData.fromView(WidgetsBinding.instance.window).size.width,
