8a48c9c8feaa653595c96dc81defa852c2b19777
DW at xps <weizhong2004@gmail.com>
2024-07-25 18:35:10 +1200

    fix: adjust floating pagination layout and style

--- lib/screens/components/floating_pagination.dart
+++ lib/screens/components/floating_pagination.dart
@@ -1,4 +1,3 @@
-// lib/floating_pagination.dart
 import 'package:flutter/material.dart';
 import 'page_selector.dart';
 
@@ -37,59 +36,70 @@ class FloatingPaginationState extends State<FloatingPagination> {
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      right: 16,
+      right: 6,
       top: 100,
       bottom: 100,
       child: showPageSelector
           ? PageSelector(
-        totalPages: widget.totalPages,
-        onPageSelected: _handlePageSelected,
-        onCancel: _handleCancel,
-      )
-          : Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          GestureDetector(
-            onLongPress: () {
-              setState(() {
-                showPageSelector = true;
-              });
-            },
-            child: FloatingActionButton(
-              heroTag: 'prevPage',
-              mini: true,
-              onPressed: widget.currentPage == 1
-                  ? null
-                  : () => widget.navigateToPage(widget.currentPage - 1),
-              backgroundColor: widget.currentPage == 1
-                  ? Colors.grey.shade400
-                  : const Color(0xFFEBDDFF),
-              child: const Icon(Icons.arrow_upward),
+              totalPages: widget.totalPages,
+              onPageSelected: _handlePageSelected,
+              onCancel: _handleCancel,
+            )
+          : GestureDetector(
+              onLongPress: () {
+                setState(() {
+                  showPageSelector = true;
+                });
+              },
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Opacity(
+                    opacity: 0.5,
+                    child: FloatingActionButton(
+                      heroTag: 'prevPage',
+                      mini: true,
+                      onPressed: widget.currentPage == 1 ? null : () => widget.navigateToPage(widget.currentPage - 1),
+                      backgroundColor: widget.currentPage == 1 ? Colors.grey.shade400 : const Color(0xFFEBDDFF),
+                      child: const Icon(Icons.arrow_upward),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 4.0,
+                      horizontal: 8.0,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlue.withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    child: Text(
+                      '${widget.currentPage}',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Opacity(
+                    opacity: 0.5,
+                    child: FloatingActionButton(
+                      heroTag: 'nextPage',
+                      mini: true,
+                      onPressed: widget.currentPage == widget.totalPages
+                          ? null
+                          : () => widget.navigateToPage(widget.currentPage + 1),
+                      backgroundColor:
+                          widget.currentPage == widget.totalPages ? Colors.grey.shade400 : const Color(0xFFEBDDFF),
+                      child: const Icon(Icons.arrow_downward),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(height: 16),
-          Text('${widget.currentPage}'),
-          const SizedBox(height: 16),
-          GestureDetector(
-            onLongPress: () {
-              setState(() {
-                showPageSelector = true;
-              });
-            },
-            child: FloatingActionButton(
-              heroTag: 'nextPage',
-              mini: true,
-              onPressed: widget.currentPage == widget.totalPages
-                  ? null
-                  : () => widget.navigateToPage(widget.currentPage + 1),
-              backgroundColor: widget.currentPage == widget.totalPages
-                  ? Colors.grey.shade400
-                  : const Color(0xFFEBDDFF),
-              child: const Icon(Icons.arrow_downward),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
