03667196b7ddc5c3e3129d9cbfd08fce1604ff0e
DW at xps <weizhong2004@gmail.com>
2024-08-14 19:54:48 +1200

    Revert "refactor: update onPopInvoked to onPopInvokedWithResult in NoteDetailState"
    This reverts commit 691cfbfbca9d259ba44a38d7b02a262ec5d71c81.

--- lib/screens/note_detail/note_detail.dart
+++ lib/screens/note_detail/note_detail.dart
@@ -71,7 +71,7 @@ class NoteDetailState extends State<NoteDetail> with RouteAware {
         builder: (context, child) {
           return PopScope(
             canPop: false,
-            onPopInvokedWithResult: (didPop, result) => _controller.onPopHandler(context, didPop),
+            onPopInvoked: (didPop) => _controller.onPopHandler(context, didPop),
             child: Scaffold(
               appBar: AppBar(
                 title: Text('Note ${note?.id}'),
