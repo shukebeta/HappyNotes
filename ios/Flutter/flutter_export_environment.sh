32a8b587a06cb4ec9673811a9220d7f8235ab79e
davidwei <davidwei@xps>
2025-07-27 21:28:37 +1200

    Update Flutter SDK path and dependency versions

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/davidwei/snap/flutter/common/flutter"
+export "FLUTTER_ROOT=/home/davidwei/Android/flutter/flutter"
 export "FLUTTER_APPLICATION_PATH=/home/davidwei/AndroidStudioProjects/happy_notes"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
