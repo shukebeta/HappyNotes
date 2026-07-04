f7047b5812f0255472682f127fdcc1319017af32
shukebeta <shukebeta@users.noreply.github.com>
2026-07-04 12:35:13 +0000

    deploy: e1949c4596337ed605e39b56778f326cbeeb4e05

--- staging/flutter_bootstrap.js
+++ staging/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"cafcda5721a78a7884db92f13c5e89f7643d52
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2968992766"
+    serviceWorkerVersion: "313351866"
   }
 });
